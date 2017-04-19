f20bc9498e22b96bab7cd3b0d88c805f98bda951
LuboZl <zlatohlavek.lubos@gmail.com>
2017-04-19 08:36:52 +0200

    test

--- app.js
+++ app.js
@@ -13,6 +13,6 @@ appPassword: 'ee0PmtyOmVN8vQXZ1NCMf3R'});
 var bot = new builder.UniversalBot(connector); server.post('/api/messages', connector.listen());
 //========================================================= // Bots Dialogs //=========================================================
    bot.dialog('/', function (session) {
-       session.send("Hello World");
+       session.send("KKT");
 });
  
